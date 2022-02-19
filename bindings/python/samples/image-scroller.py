cb53b35a533698967df692f596815a081f6086ce
Patrick Bullion <pbullion@gmail.com>
2022-02-18 18:24:57 -0600

    updates

--- bindings/python/samples/image-scroller.py
+++ bindings/python/samples/image-scroller.py
@@ -3,6 +3,7 @@ import time
 from samplebase import SampleBase
 from PIL import Image
 import requests, json
+import feedparser
 
 class ImageScroller(SampleBase):
     def __init__(self, *args, **kwargs):
@@ -11,7 +12,7 @@ class ImageScroller(SampleBase):
 
     def run(self):
         print('++++++++++++++++++++++++++++')
-        NewsFeed = self.parser("https://www.rotowire.com/rss/news.php?sport=NBA")
+        NewsFeed = feedparser.parse("https://www.rotowire.com/rss/news.php?sport=NBA")
         entry = NewsFeed.entries[1]
         print(entry)
         # url = requests.get("https://www.rotowire.com/rss/news.php?sport=NBA")
