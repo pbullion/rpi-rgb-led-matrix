838a664a69b408b908ee25e3a96013ad958b6f4f
Patrick Bullion <pbullion@gmail.com>
2022-02-18 21:57:59 -0600

    updates

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -19,7 +19,7 @@ class RunText(SampleBase):
         print
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
-        font.LoadFont("../../../fonts/7x13.bdf")
+        font.LoadFont("../../../fonts/9x18.bdf")
         textColor = graphics.Color(255, 255, 0)
         pos = offscreen_canvas.width
         my_text = self.args.text
