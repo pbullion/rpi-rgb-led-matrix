51072f6103689fb4ccdad3cb1847d6808683f177
Patrick Bullion <pbullion@gmail.com>
2022-02-23 22:39:32 -0600

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -4,7 +4,6 @@ from samplebase import SampleBase
 from rgbmatrix import graphics
 import time
 import requests, json
-import asyncio
 
 class RunText(SampleBase):
     def __init__(self, *args, **kwargs):
@@ -12,11 +11,11 @@ class RunText(SampleBase):
         super(RunText, self).__init__(*args, **kwargs)
         self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
 
-    async def run(self):
+    def run(self):
         print('++++++++++++++++++++++++++++')
         url = requests.get("https://sheline-art-website-api.herokuapp.com/patrick/espn")
         text = url.text
-        # print(text)
+        print(text)
         print
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
@@ -28,25 +27,19 @@ class RunText(SampleBase):
         strings = ['Helllllllllllllllllllllo mutha fucka','tits fart turd and twat']
 
         while True:
-            offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 24, red, strings[0])
-            pos -= 1
-            if (pos + len < 0):
-                pos = offscreen_canvas.width
-            time.sleep(0.02)
-            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
-            await asyncio.sleep(5)
-            offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 24, green, strings[1])
-            pos -= 1
-            if (pos + len < 0):
-                pos = offscreen_canvas.width
-            time.sleep(0.02)
-            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+            for string in strings:
+                offscreen_canvas.Clear()
+                len = graphics.DrawText(offscreen_canvas, font, pos, 24, green, string)
+                pos -= 1
+                if (pos + len < 0):
+                    pos = offscreen_canvas.width
+                time.sleep(0.02)
+                offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+
 
 
 # Main function
 if __name__ == "__main__":
-    run_text = asyncio.run(RunText())
+    run_text = RunText()
     if (not run_text.process()):
         run_text.print_help()
