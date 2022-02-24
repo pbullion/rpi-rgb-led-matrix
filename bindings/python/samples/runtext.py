160b37aed0d0a2a4c85e7d48c2bb878353f285b9
Patrick Bullion <pbullion@gmail.com>
2022-02-23 22:47:12 -0600

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -26,22 +26,18 @@ class RunText(SampleBase):
         # my_text = text
         strings = ['Helllllllllllllllllllllo mutha fucka','tits fart turd and twat']
 
+        async def getScrollingText():
+            offscreen_canvas.Clear()
+            len = graphics.DrawText(offscreen_canvas, font, pos, 24, green, string)
+            pos -= 1
+            if (pos + len < 0):
+                pos = offscreen_canvas.width
+            time.sleep(0.02)
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+
         while True:
             for string in strings:
-                offscreen_canvas.Clear()
-                len = graphics.DrawText(offscreen_canvas, font, pos, 24, green, string)
-                pos -= 1
-                if (pos + len < 0):
-                    pos = offscreen_canvas.width
-                time.sleep(0.02)
-                offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
-                offscreen_canvas.Clear()
-                len = graphics.DrawText(offscreen_canvas, font, pos, 24, red, string)
-                pos -= 1
-                if (pos + len < 0):
-                    pos = offscreen_canvas.width
-                time.sleep(0.02)
-                offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+                yield getScrollingText()
 
 
 
