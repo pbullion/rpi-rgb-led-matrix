050fa3e05ba8d747abca22132e877860d8175b68
Patrick Bullion <pbullion@gmail.com>
2022-02-23 22:06:23 -0600

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -4,7 +4,6 @@ from samplebase import SampleBase
 from rgbmatrix import graphics
 import time
 import requests, json
-import asyncio
 
 class RunText(SampleBase):
     def __init__(self, *args, **kwargs):
@@ -25,20 +24,17 @@ class RunText(SampleBase):
         red = graphics.Color(255, 0, 0)
         pos = offscreen_canvas.width
         # my_text = text
-        strings = ['Helllllllllllllllllllllo mutha fucka','tits fart turd and twat']
-
-        async def scrollingText(string, color):
-            offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 24, color, string)
-            pos -= 1
-            if (pos + len < 0):
-                pos = offscreen_canvas.width
-            time.sleep(0.02)
-            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+        my_text = ['Helllllllllllllllllllllo mutha fucka','tits fart turd and twat']
 
         while True:
             for string in strings:
-                await scrollingText(string, green)
+                offscreen_canvas.Clear()
+                len = graphics.DrawText(offscreen_canvas, font, pos, 24, green, string)
+                pos -= 1
+                if (pos + len < 0):
+                    pos = offscreen_canvas.width
+                time.sleep(0.02)
+                offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
 
 # Main function
