690f1a63d36c9bdc47e9e78cd8dc335fa1095e2f
Patrick Bullion <pbullion@gmail.com>
2022-02-23 22:52:57 -0600

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -36,6 +36,14 @@ class RunText(SampleBase):
                         pos = offscreen_canvas.width
                     time.sleep(0.02)
                     offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+                while True:
+                    offscreen_canvas.Clear()
+                    len = graphics.DrawText(offscreen_canvas, font, pos, 24, red, string)
+                    pos -= 1
+                    if (pos + len < 0):
+                        pos = offscreen_canvas.width
+                    time.sleep(0.02)
+                    offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
 
 
