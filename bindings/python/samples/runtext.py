ff90e4c97bde00dda0e07a061a03fd4f6311ee56
Patrick Bullion <pbullion@gmail.com>
2022-02-23 21:35:26 -0600

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -24,25 +24,25 @@ class RunText(SampleBase):
         red = graphics.Color(255, 0, 0)
         pos = offscreen_canvas.width
         # my_text = text
-        my_text = 'Helllllllllllllllllllllo mutha fucka'
-        second_text = 'tits fart turd and twat'
+        my_text = ['Helllllllllllllllllllllo mutha fucka','tits fart turd and twat']
 
         while True:
-            offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 24, green, my_text)
-            pos -= 1
-            if (pos + len < 0):
-                pos = offscreen_canvas.width
-            time.sleep(0.02)
-            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+            for string in strings:
+                offscreen_canvas.Clear()
+                len = graphics.DrawText(offscreen_canvas, font, pos, 24, green, string)
+                pos -= 1
+                if (pos + len < 0):
+                    pos = offscreen_canvas.width
+                time.sleep(0.02)
+                offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
-            offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 24, red, second_text)
-            pos -= 1
-            if (pos + len < 0):
-                pos = offscreen_canvas.width
-            time.sleep(0.02)
-            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+                offscreen_canvas.Clear()
+                len = graphics.DrawText(offscreen_canvas, font, pos, 24, red, string)
+                pos -= 1
+                if (pos + len < 0):
+                    pos = offscreen_canvas.width
+                time.sleep(0.02)
+                offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
 
 # Main function
