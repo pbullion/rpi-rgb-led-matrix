cc370a6a23bbbc77b47330afc2cd9eb130ad1e57
Patrick Bullion <pbullion@gmail.com>
2022-02-23 21:36:26 -0600

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -26,23 +26,22 @@ class RunText(SampleBase):
         # my_text = text
         strings = ['Helllllllllllllllllllllo mutha fucka','tits fart turd and twat']
 
-        while True:
-            for string in strings:
-                offscreen_canvas.Clear()
-                len = graphics.DrawText(offscreen_canvas, font, pos, 24, green, string)
-                pos -= 1
-                if (pos + len < 0):
-                    pos = offscreen_canvas.width
-                time.sleep(0.02)
-                offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+        for string in strings:
+            offscreen_canvas.Clear()
+            len = graphics.DrawText(offscreen_canvas, font, pos, 24, green, string)
+            pos -= 1
+            if (pos + len < 0):
+                pos = offscreen_canvas.width
+            time.sleep(0.02)
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
-                offscreen_canvas.Clear()
-                len = graphics.DrawText(offscreen_canvas, font, pos, 24, red, string)
-                pos -= 1
-                if (pos + len < 0):
-                    pos = offscreen_canvas.width
-                time.sleep(0.02)
-                offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+            offscreen_canvas.Clear()
+            len = graphics.DrawText(offscreen_canvas, font, pos, 24, red, string)
+            pos -= 1
+            if (pos + len < 0):
+                pos = offscreen_canvas.width
+            time.sleep(0.02)
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
 
 # Main function
