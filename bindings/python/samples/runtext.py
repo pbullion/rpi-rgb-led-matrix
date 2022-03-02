8638530c4be4231a84b5661877523e1a25d6d706
Patrick Bullion <pbullion@gmail.com>
2022-03-01 21:09:15 -0600

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -32,6 +32,8 @@ class RunText(SampleBase):
             yellow = graphics.Color(255, 255, 0)
             colors = [blue, teal, purple, yellow]
             pos = offscreen_canvas.width
+            gamePos1 = offscreen_canvas.width
+            gamePos2 = offscreen_canvas.width
             randomNum = random.randint(0,3)
             for string in strings:
                 running = True
