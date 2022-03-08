60706b0f0ffedb5c10f3e07badc6758789fa3ec8
Patrick Bullion <pbullion@gmail.com>
2022-03-08 17:29:24 -0600

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -14,17 +14,23 @@ class RunText(SampleBase):
         super(RunText, self).__init__(*args, **kwargs)
         self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
         self.parser.add_argument("-i", "--image", help="The image to display", default="../../../examples-api-use/runtext.ppm")
-        
+
     def run(self):
-        self.counter = 0   
         if not 'image' in self.__dict__:
-            self.image = Image.open(self.args.image)
-            print("hello i did it")
+            self.image = Image.open(self.args.image).convert('RGB')
         self.image.resize((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
-        offscreen_canvas = self.matrix.CreateFrameCanvas()
-        pos = offscreen_canvas.width
 
         while True:
+            print('++++++++++++++++++++++++++++')
+            url = requests.get("https://sheline-art-website-api.herokuapp.com/patrick/all-data/pbullion@gmail.com")
+            strings = json.loads(url.text)
+            print(strings)
+            offscreen_canvas = self.matrix.CreateFrameCanvas()
+            img_width, img_height = self.image.size
+            font = graphics.Font()
+            font.LoadFont("../../../fonts/texgyre-27.bdf")
+            gameFont = graphics.Font()
+            gameFont.LoadFont("../../../fonts/6x13.bdf")
             green = graphics.Color(0, 255, 0)
             red = graphics.Color(255, 0, 0)
             blue = graphics.Color(0, 0, 255)
@@ -32,17 +38,10 @@ class RunText(SampleBase):
             purple = graphics.Color(102, 0, 204)
             yellow = graphics.Color(255, 255, 0)
             colors = [blue, teal, purple, yellow]
-            print('++++++++++++++++++++++++++++')
-            url = requests.get("https://sheline-art-website-api.herokuapp.com/patrick/all-data/pbullion@gmail.com")
-            strings = json.loads(url.text)
-            print(strings)
-            # offscreen_canvas = self.matrix.CreateFrameCanvas()
-            font = graphics.Font()
-            font.LoadFont("../../../fonts/texgyre-27.bdf")
-            gameFont = graphics.Font()
-            gameFont.LoadFont("../../../fonts/6x13.bdf")
-            randomNum = random.randint(0,3)
             pos = offscreen_canvas.width
+            gamePos1 = offscreen_canvas.width
+            gamePos2 = offscreen_canvas.width
+            randomNum = random.randint(0,3)
             for string in strings:
                 running = True
                 if 'POLL' in string:
@@ -59,22 +58,18 @@ class RunText(SampleBase):
                     color = green
                 else:
                     color = blue
-                length = 1
+                len = 1
                 while running:
                     offscreen_canvas.Clear()
-                    img_width, img_height = self.image.size
-                    len = graphics.DrawText(offscreen_canvas, font, pos, 22, color, string) + img_width
+                    len = graphics.DrawText(offscreen_canvas, font, pos, 24, color, string) + img_width
                     pos -= 1
-                    if (self.counter < 2):
-                        self.counter += 1
-                    else:
-                        self.counter = 0
-                        print(self.counter)
+                    if (pos + len < 0):
+                        running = False
                         pos = offscreen_canvas.width
-                        run_text.run()
                     time.sleep(0.01)
+                    double_buffer.SetImage(self.image, pos)
+                    double_buffer.SetImage(self.image, pos + img_width)
                     offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
-
 
 
 
