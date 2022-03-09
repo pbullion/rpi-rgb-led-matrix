b01c8f273f845f9269889a9cd922c820b8709f9c
Patrick Bullion <pbullion@gmail.com>
2022-03-08 21:12:53 -0600

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -13,7 +13,7 @@ class RunText(SampleBase):
         print(self)
         super(RunText, self).__init__(*args, **kwargs)
         self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
-        self.parser.add_argument("-i", "--image", help="The image to display", default="./tesla.png")
+        self.parser.add_argument("-i", "--image", help="The image to display", default="./tesla1.png")
 
     def run(self):
         if not 'image' in self.__dict__:
