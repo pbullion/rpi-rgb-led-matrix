af3d8f3d05f0a2de6a0d652226d9035e2b0afcd7
Patrick Bullion <pbullion@gmail.com>
2022-03-08 18:16:54 -0600

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -1,83 +1,85 @@
 #!/usr/bin/env python
-#Display a runtext with double-buffering.
-#path = cd /home/pi/rpi-rgb-led-matrix/bindings/python/samples
-#run = sudo python stones.py --led-slowdown-gpio=4 --led-cols=64 --led-chain=8
+# Display a runtext with double-buffering.
 from samplebase import SampleBase
 from rgbmatrix import graphics
 import time
-import multiprocessing
-import yfinance as yf
-import threading
+import requests, json
+import json
+import random
 from PIL import Image
 
 class RunText(SampleBase):
-    def init(self, *args, **kwargs):
-        super(RunText, self).init(*args, **kwargs)
-        #new
+    def __init__(self, *args, **kwargs):
+        print(self)
+        super(RunText, self).__init__(*args, **kwargs)
         self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
-        self.parser.add_argument("-i", "--image", help="The image to display", default="../../../examples-api-use/nvdia.ppm" )
-        #ticker data holder
-        self.tickerData = {}
-        #list of tickers
-        self.tickers = ["MSFT", "NVDA", "DIS"]
-
-        self.maxTickers = len(self.tickers)
+        self.parser.add_argument("-i", "--image", help="The image to display", default="../../../examples-api-use/runtext.ppm")
+        self.counter = 0   
         
-        self.counter = 0
-    
-    
-def updateTickerData(self, i):
-    #ticker data
-    data = yf.Ticker(self.tickers[self.counter])
-    self.greeting = str(data.info['regularMarketOpen'])
-    #fun.greeting = str(fun.counter) (maybe don't need)
-    
-    #print(fun.tickers[fun.counter] + " is valued at " + "$" + str(current.info['regularMarketOpen']))
-    #threading.Thread(target=getTicker).start()
-   
-def run(self):
-    if not 'image' in self.__dict__:
-        self.image = Image.open(self.args.image)
-        print("hello i did it")
-    self.image.resize((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
-    
-    offscreen_canvas = self.matrix.CreateFrameCanvas()
-    
-    #self.data
-    font = graphics.Font()
-    font.LoadFont("../../../fonts/aver-20.bdf")
-    textColor = graphics.Color(255, 255, 255)
-    pos = offscreen_canvas.width
-    data = yf.Ticker(self.tickers[self.counter])
-    self.greeting = "THE PRICE OF " + self.tickers[self.counter] + " IS " + "!!!" + str(data.info['regularMarketOpen'])
-    print(self.greeting)
-    my_text = self.greeting
-    #self.image
-    
-    while True:
-        offscreen_canvas.Clear()
-        img_width, img_height = self.image.size
-        len = graphics.DrawText(offscreen_canvas, font, pos, 22, textColor, my_text) + img_width
-        pos -= 1
-        if (pos + len < 0):
-            if (self.counter < 2):
-                self.counter += 1
-            else:
-                self.counter = 0
-            print(self.counter)
+    def run(self):
+        if not 'image' in self.__dict__:
+            self.image = Image.open(self.args.image)
+            print("hello i did it")
+        self.image.resize((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
+        offscreen_canvas = self.matrix.CreateFrameCanvas()
+        pos = offscreen_canvas.width
+
+        while True:
+            green = graphics.Color(0, 255, 0)
+            red = graphics.Color(255, 0, 0)
+            blue = graphics.Color(0, 0, 255)
+            teal = graphics.Color(0, 255, 255)
+            purple = graphics.Color(102, 0, 204)
+            yellow = graphics.Color(255, 255, 0)
+            colors = [blue, teal, purple, yellow]
+            print('++++++++++++++++++++++++++++')
+            url = requests.get("https://sheline-art-website-api.herokuapp.com/patrick/all-data/pbullion@gmail.com")
+            strings = json.loads(url.text)
+            print(strings)
+            # offscreen_canvas = self.matrix.CreateFrameCanvas()
+            font = graphics.Font()
+            font.LoadFont("../../../fonts/texgyre-27.bdf")
+            gameFont = graphics.Font()
+            gameFont.LoadFont("../../../fonts/6x13.bdf")
+            randomNum = random.randint(0,3)
             pos = offscreen_canvas.width
-            
-            #offscreen_canvas.Clear()
-            run_text.run()
-            # self.cryptoData = self.return_dict['data']
-            
-            
+            for string in strings:
+                running = True
+                if 'POLL' in string:
+                    color = green
+                elif 'BAYLOR' in string:
+                    color = green
+                elif 'CURRENT WEATHER' in string:
+                    color = colors[randomNum]
+                elif '#' in string in string:
+                    color = green
+                elif '-' in string in string:
+                    color = red
+                elif '+' in string in string:
+                    color = green
+                else:
+                    color = blue
+                length = 1
+                while running:
+                    offscreen_canvas.Clear()
+                    img_width, img_height = self.image.size
+                    len = graphics.DrawText(offscreen_canvas, font, pos, 22, color, string) + img_width
+                    pos -= 1
+                    if (self.counter < 2):
+                        self.counter += 1
+                    else:
+                        self.counter = 0
+                        print(self.counter)
+                        pos = offscreen_canvas.width
+                        run_text.run()
+                    time.sleep(0.01)
+                    offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
-        time.sleep(.05)
 
-        offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
-#Main function
-if name == "main":
+
+
+# Main function
+if __name__ == "__main__":
     run_text = RunText()
     if (not run_text.process()):
         run_text.print_help()
