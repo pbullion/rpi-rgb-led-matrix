18c50b1fb469455edf89d169bbb13a7689723bc2
Patrick Bullion <pbullion@gmail.com>
2022-03-09 20:45:03 -0600

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -83,7 +83,7 @@ class RunText(SampleBase):
                         offscreen_canvas.SetImage(partlyCloudyImage, pos)
                         len = graphics.DrawText(offscreen_canvas, font, pos + partlyCloudyImage.width, 24, color, string)
                         time.sleep(0.01)
-                    elif '-' and '%' for x in string:
+                    elif '-' in string[1]:
                         print('===---------=======')
                         print(stockLogo)
                         img_width, img_height = stockLogo.size
