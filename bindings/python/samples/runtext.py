c00b811edc7acdfd5b1ed0ae42570374baac8655
Patrick Bullion <pbullion@gmail.com>
2022-03-14 21:30:39 -0500

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -161,21 +161,21 @@ class RunText(SampleBase):
                         color = green
                         basketballLogo = Image.open('./images/logos/basketball.png').convert('RGB').resize((32,32), Image.ANTIALIAS)
                         pos -= 1
-                        if (pos + basketballLogo.width + len < 0):
+                        if (pos + len < 0):
                             running = False
                             pos = offscreen_canvas.width
                         # offscreen_canvas.SetImage(basketballLogo, pos)
-                        len = graphics.DrawText(offscreen_canvas, font, pos + basketballLogo.width + 4, 24, color, string)
+                        len = graphics.DrawText(offscreen_canvas, font, pos + 4, 24, color, string)
                         time.sleep(0.01)
                     elif 'ESPN' in string:
                         color = green
                         espnLogo = Image.open('./images/logos/espn.png').convert('RGB').resize((130,32), Image.ANTIALIAS)
                         pos -= 1
-                        if (pos + espnLogo.width + len < 0):
+                        if (pos +  len < 0):
                             running = False
                             pos = offscreen_canvas.width
                         # offscreen_canvas.SetImage(espnLogo, pos)
-                        len = graphics.DrawText(offscreen_canvas, font, pos + espnLogo.width + 4, 24, color, string)
+                        len = graphics.DrawText(offscreen_canvas, font, pos + 4, 24, color, string)
                         time.sleep(0.01)
                     elif 'FOXNEWS' in string:
                         color = blue
