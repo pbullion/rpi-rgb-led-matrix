e27c4e112f2f642949105377be38894354c9a82b
Patrick Bullion <pbullion@gmail.com>
2022-03-14 21:42:09 -0500

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -165,7 +165,7 @@ class RunText(SampleBase):
                         time.sleep(0.01)
                     elif 'ESPN' in string:
                         color = green
-                        espnLogo = Image.open('./images/logos/espnLogo.png').convert('RGB').resize((130,32), Image.ANTIALIAS)
+                        espnLogo = Image.open('./images/logos/espnLogo.png').convert('RGB').resize((32,32), Image.ANTIALIAS)
                         pos -= 1
                         if (pos + espnLogo.width + len < 0):
                             running = False
