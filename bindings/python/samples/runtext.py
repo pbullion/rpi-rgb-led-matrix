a4977c6cedcb5245cc2ce221abea4811ec0ff124
Patrick Bullion <pbullion@gmail.com>
2022-03-15 17:19:30 -0500

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -128,10 +128,10 @@ class RunText(SampleBase):
                         offscreen_canvas.SetImage(homeLogo, pos + awayLogo.width + buffer + buffer + awayTeam + versus)
 
                         homeTeam = graphics.DrawText(offscreen_canvas, smallFont, pos + awayLogo.width + buffer + buffer + awayTeam + versus + buffer + homeLogo.width, 10, homeColor, homeTeamString)
-                        homeTeamStatus = graphics.DrawText(offscreen_canvas, smallFont, pos + awayLogo.width + buffer + buffer + awayTeam + versus + buffer + homeLogo.width + 10, 22, awayColor, awayTeamStatusString)
+                        homeTeamStatus = graphics.DrawText(offscreen_canvas, smallFont, pos + awayLogo.width + buffer + buffer + awayTeam + versus + buffer + homeLogo.width + 10, 22, homeColor, awayTeamStatusString)
                         
                         status = graphics.DrawText(offscreen_canvas, smallFont, pos + awayLogo.width + buffer + buffer + awayTeam + versus + buffer + homeLogo.width + homeTeam + buffer, 24, purple, statusString)
-                        if (pos + awayLogo.width + buffer + buffer + awayTeam + versus + buffer + homeLogo.width + homeTeam + buffer < 0):
+                        if (pos + awayLogo.width + buffer + buffer + awayTeam + versus + buffer + homeLogo.width + homeTeam + buffer + 10 < 0):
                             running = False
                             pos = offscreen_canvas.width
                         time.sleep(0.01)
