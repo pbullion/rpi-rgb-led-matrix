bcd41e86e16fe70dbcb345ab1c80cd9442fcad22
Patrick Bullion <pbullion@gmail.com>
2022-03-17 13:57:58 -0500

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -148,7 +148,7 @@ class RunText(SampleBase):
                             status = graphics.DrawText(offscreen_canvas, middleFont, pos + awayLogo.width + buffer + buffer + awayTeam, 26, green, statusString)
                             offscreen_canvas.SetImage(homeLogo, pos + awayLogo.width + buffer + buffer + awayTeam + status + buffer + buffer + buffer)
                             homeTeam = graphics.DrawText(offscreen_canvas, smallFont, pos + awayLogo.width + buffer + buffer + awayTeam + status + buffer + buffer + buffer + buffer + homeLogo.width, 10, homeColor, homeTeamString)
-                            homeCentered = homeTeam / 3
+                            homeCentered = homeTeam / 2
                             homeTeamStatus = graphics.DrawText(offscreen_canvas, font, pos + awayLogo.width + buffer + buffer + awayTeam + status + buffer + homeLogo.width + homeCentered, 31, homeColor, homeTeamStatusString)
                             if (pos + awayLogo.width + buffer + buffer + awayTeam + status + buffer + homeLogo.width + homeTeam + buffer + status < 0):
                                 running = False
