b388f6de4966fad05ea4fefa0e9fe5af450e80f2
Patrick Bullion <pbullion@gmail.com>
2022-03-17 13:53:21 -0500

    up

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -140,11 +140,11 @@ class RunText(SampleBase):
                             awayTeamStatus = graphics.DrawText(offscreen_canvas, font, pos + awayLogo.width + buffer + awayCentered, 31, awayColor, awayTeamStatusString)
                             quarter = graphics.DrawText(offscreen_canvas, middleFont, pos + awayLogo.width + buffer + buffer + awayTeam + 4, 12, green, oddsString)
                             status = graphics.DrawText(offscreen_canvas, middleFont, pos + awayLogo.width + buffer + buffer + awayTeam, 26, green, statusString)
-                            offscreen_canvas.SetImage(homeLogo, pos + awayLogo.width + buffer + buffer + awayTeam + quarter + buffer + buffer + buffer)
-                            homeTeam = graphics.DrawText(offscreen_canvas, smallFont, pos + awayLogo.width + buffer + buffer + awayTeam + quarter + buffer + homeLogo.width, 10, homeColor, homeTeamString)
+                            offscreen_canvas.SetImage(homeLogo, pos + awayLogo.width + buffer + buffer + awayTeam + status + buffer + buffer + buffer)
+                            homeTeam = graphics.DrawText(offscreen_canvas, smallFont, pos + awayLogo.width + buffer + buffer + awayTeam + status + buffer + homeLogo.width, 10, homeColor, homeTeamString)
                             homeCentered = homeTeam / 3
-                            homeTeamStatus = graphics.DrawText(offscreen_canvas, font, pos + awayLogo.width + buffer + buffer + awayTeam + quarter + buffer + homeLogo.width + homeCentered, 31, homeColor, homeTeamStatusString)
-                        if (pos + awayLogo.width + buffer + buffer + awayTeam + quarter + buffer + homeLogo.width + homeTeam + buffer + quarter < 0):
+                            homeTeamStatus = graphics.DrawText(offscreen_canvas, font, pos + awayLogo.width + buffer + buffer + awayTeam + status + buffer + homeLogo.width + homeCentered, 31, homeColor, homeTeamStatusString)
+                        if (pos + awayLogo.width + buffer + buffer + awayTeam + status + buffer + homeLogo.width + homeTeam + buffer + status < 0):
                             running = False
                             pos = offscreen_canvas.width
                         time.sleep(0.01)
