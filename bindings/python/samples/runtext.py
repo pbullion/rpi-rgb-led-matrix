6a3e3be662be9de20a5fbbd567ffcf7768d1f320
Patrick Bullion <pbullion@gmail.com>
2022-03-22 18:01:55 -0500

    u

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -159,7 +159,7 @@ class RunText(SampleBase):
                             homeTeamStatus = graphics.DrawText(offscreen_canvas, font, pos + runningCount + awayLogo.width + buffer + buffer + awayTeam + quarter + buffer + homeLogo.width + homeCentered, 31, homeColor, homeTeamStatusString)
                             runningCount = runningCount + pos + awayLogo.width + buffer + buffer + awayTeam + status + buffer + homeLogo.width + homeTeam + buffer + quarter 
                             time.sleep(0.008)
-                        if (runningCount < 0):
+                        if (runningCount < pos):
                             running = False
                             pos = offscreen_canvas.width
                     # elif isinstance(string, list) and 'game' in string[0]:
