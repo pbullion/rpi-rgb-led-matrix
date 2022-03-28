1275c2e7d7f2878d98ed05cd69c72ee67ab055ba
Patrick Bullion <pbullion@gmail.com>
2022-03-28 14:14:04 -0500

    u

--- bindings/python/samples/tinyLED.py
+++ bindings/python/samples/tinyLED.py
@@ -54,8 +54,8 @@ class RunText(SampleBase):
                     homeColorSecondary = graphics.Color(item['homeTeam']['colors']['secondary'][0],item['homeTeam']['colors']['secondary'][1],item['homeTeam']['colors']['secondary'][2])
                     if item['pregame'] == False:    
                         # canvas.Fill(item['homeTeam']['colors']['secondary'][0],item['homeTeam']['colors']['secondary'][1],item['homeTeam']['colors']['secondary'][2])          
-                        awayTeam = graphics.DrawText(canvas, smallFont, 1, 12, awayColorPrimary, item['awayTeam']['name'])
-                        homeTeam = graphics.DrawText(canvas, smallFont, 1, 24, homeColorPrimary, item['homeTeam']['name'])
+                        awayTeam = graphics.DrawText(canvas, smallFont, 1, 12, awayColorPrimary, awayColorSecondary, item['awayTeam']['name'])
+                        homeTeam = graphics.DrawText(canvas, smallFont, 1, 24, homeColorPrimary, homeColorSecondary, item['homeTeam']['name'])
                         print('######################')
                         print(awayTeam)
                         print(homeTeam)
