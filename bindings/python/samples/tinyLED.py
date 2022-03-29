1b75e3f4412b2501d46f43c8b823b173357cbd75
Patrick Bullion <pbullion@gmail.com>
2022-03-29 17:41:35 -0500

    u

--- bindings/python/samples/tinyLED.py
+++ bindings/python/samples/tinyLED.py
@@ -186,7 +186,7 @@ class RunText(SampleBase):
                         highLow = graphics.DrawText(canvas, smallestFont, runningX, 40, blue, day['highLow'])
                         runningX = runningX + 20
                 elif type(item) is dict and item['tourneyName'] != type(None):
-                    for page in item['players']
+                    for page in item['players']:
                         for player in page:
                             tourneyName = graphics.DrawText(canvas, smallestFont, 10, 5, blue, item['tourneyName'])
                             tourneyStatus = graphics.DrawText(canvas, smallestFont, 10, 12, blue, item['tourneyStatus'])
