22c0a65bdbc4f262ac898e190aede7f26367b6ff
Patrick Bullion <pbullion@gmail.com>
2022-03-29 20:37:25 -0500

    u

--- bindings/python/samples/tinyLED.py
+++ bindings/python/samples/tinyLED.py
@@ -183,13 +183,13 @@ class RunText(SampleBase):
                     stockLogo = Image.open(requests.get(item['url'], stream=True).raw).convert('RGB').resize((20,20), Image.ANTIALIAS)
                     canvas.SetImage(stockLogo, 2, 3)
                     stockSymbol = graphics.DrawText(canvas, middleFont, 25, 12, color, item['stockSymbol'])
-                    currentPrice = graphics.DrawText(canvas, smallestFont, 27, 19, color, item['currentPrice'])
-                    x = 27
+                    currentPrice = graphics.DrawText(canvas, smallestFont, 30, 19, color, item['currentPrice'])
+                    x = 26
                     y = 24 if item['up'] else 20
                     size = 4
                     for offset in range(size):
                         graphics.DrawLine(canvas, x - offset, y + (offset * direction), x + offset, y + (offset * direction), color)
-                    percentChange = graphics.DrawText(canvas, smallestFont, 30, 26, color, item['percentChange'])
+                    percentChange = graphics.DrawText(canvas, smallFont, 30, 26, color, item['percentChange'])
                 elif type(item) is dict and 'condition' in item.keys() :
                     locationString = '/home/pi/new/rpi-rgb-led-matrix/bindings/python/samples/images/day/{}.png'.format(item['icon'])
                     weatherImage = Image.open(locationString).convert('RGB').resize((32, 32), Image.ANTIALIAS)
