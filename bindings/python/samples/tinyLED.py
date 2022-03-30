83d322069d33c316f439887cf8abcb6536ea8363
Patrick Bullion <pbullion@gmail.com>
2022-03-29 19:18:05 -0500

    u

--- bindings/python/samples/tinyLED.py
+++ bindings/python/samples/tinyLED.py
@@ -180,16 +180,16 @@ class RunText(SampleBase):
                 elif type(item) is dict and 'stockSymbol' in item.keys() :
                     color = green if item['up'] else red
                     direction = 1 if item['up'] else -1
-                    stockLogo = Image.open(requests.get(item['url'], stream=True).raw).convert('RGB').resize((16,16), Image.ANTIALIAS)
-                    canvas.SetImage(stockLogo, 5, 8)
-                    stockSymbol = graphics.DrawText(canvas, smallFont, 30, 8, color, item['stockSymbol'])
-                    currentPrice = graphics.DrawText(canvas, smallestFont, 30, 19, color, item['currentPrice'])
+                    stockLogo = Image.open(requests.get(item['url'], stream=True).raw).convert('RGB').resize((24,24), Image.ANTIALIAS)
+                    canvas.SetImage(stockLogo, 2, 3)
+                    stockSymbol = graphics.DrawText(canvas, smallFont, 30, 2, color, item['stockSymbol'])
+                    currentPrice = graphics.DrawText(canvas, smallFont, 30, 19, color, item['currentPrice'])
                     x = 25
-                    y = 35
-                    size = 6
+                    y = 26
+                    size = 3
                     for offset in range(size):
                         graphics.DrawLine(canvas, x - offset, y + (offset * direction), x + offset, y + (offset * direction), color)
-                    percentChange = graphics.DrawText(canvas, smallestFont, 30, 26, color, item['percentChange'])
+                    percentChange = graphics.DrawText(canvas, smallFont, 30, 26, color, item['percentChange'])
                 elif type(item) is dict and 'condition' in item.keys() :
                     locationString = '/home/pi/new/rpi-rgb-led-matrix/bindings/python/samples/images/day/{}.png'.format(item['icon'])
                     weatherImage = Image.open(locationString).convert('RGB').resize((32, 32), Image.ANTIALIAS)
