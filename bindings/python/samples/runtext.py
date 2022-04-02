709d2136a59e8003815e4167a4024cbc7959c81e
Patrick Bullion <pbullion@gmail.com>
2022-04-02 16:00:50 -0500

    u

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -57,8 +57,8 @@ class RunText(SampleBase):
                 if string == None:
                     print('it was NONE')
                 elif isinstance(string, list) and 'inprogress mlb' in string[0]:
-                    awayLogo = Image.open(requests.get('https://loodibee.com/wp-content/uploads/mlb-new-york-yankees-logo.png', stream=True).raw).convert('RGB').resize((32,32), Image.ANTIALIAS)
-                    homeLogo = Image.open(requests.get('https://loodibee.com/wp-content/uploads/mlb-houston-astros-logo.png', stream=True).raw).convert('RGB').resize((32,32), Image.ANTIALIAS)
+                    awayLogo = Image.open(requests.get('https://loodibee.com/wp-content/uploads/mlb-{}-logo.png'.format(string[23]), stream=True).raw).convert('RGB').resize((32,32), Image.ANTIALIAS)
+                    homeLogo = Image.open(requests.get('https://loodibee.com/wp-content/uploads/mlb-{}-logo.png'.format(string[24]), stream=True).raw).convert('RGB').resize((32,32), Image.ANTIALIAS)
                     # awayLogo = Image.open(requests.get(string[1], stream=True).raw).convert('RGB').resize((32,32), Image.ANTIALIAS)
                     # homeLogo = Image.open(requests.get(string[6], stream=True).raw).convert('RGB').resize((32,32), Image.ANTIALIAS)
                     pitcherHeadshot = string[16]
@@ -66,8 +66,8 @@ class RunText(SampleBase):
                     pitcherImage = Image.open(requests.get(pitcherHeadshot, stream=True).raw).convert('RGB').resize((32,32), Image.ANTIALIAS)
                     batterImage = Image.open(requests.get(batterHeadshot, stream=True).raw).convert('RGB').resize((32,32), Image.ANTIALIAS)
                 elif isinstance(string, list) and 'game' in string[0]:
-                    awayLogo = Image.open(requests.get('https://loodibee.com/wp-content/uploads/mlb-new-york-yankees-logo.png', stream=True).raw).convert('RGB').resize((32,32), Image.ANTIALIAS)
-                    homeLogo = Image.open(requests.get('https://loodibee.com/wp-content/uploads/mlb-houston-astros-logo.png', stream=True).raw).convert('RGB').resize((32,32), Image.ANTIALIAS)
+                    awayLogo = Image.open(requests.get('https://loodibee.com/wp-content/uploads/mlb-{}-logo.png'.format(string[23]), stream=True).raw).convert('RGB').resize((32,32), Image.ANTIALIAS)
+                    homeLogo = Image.open(requests.get('https://loodibee.com/wp-content/uploads/mlb-{}-logo.png'.format(string[24]), stream=True).raw).convert('RGB').resize((32,32), Image.ANTIALIAS)
                     # awayLogo = Image.open(requests.get(string[1], stream=True).raw).convert('RGB').resize((32,32), Image.ANTIALIAS)
                     # homeLogo = Image.open(requests.get(string[6], stream=True).raw).convert('RGB').resize((32,32), Image.ANTIALIAS)
                 elif isinstance(string, list) and 'http' in string[0]:
