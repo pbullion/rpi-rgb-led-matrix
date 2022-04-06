a82ddf7515c667381edb2efa3407bce70282928e
Patrick Bullion <pbullion@gmail.com>
2022-04-05 21:56:30 -0500

    u

--- bindings/python/samples/tinyLED.py
+++ bindings/python/samples/tinyLED.py
@@ -101,12 +101,8 @@ class RunText(SampleBase):
                         awayTeamBlack = graphics.DrawText(canvas, smallFont, 2, 11, black, item['awayTeam']['teamName'])
                         homeTeamBlack = graphics.DrawText(canvas, smallFont, 2, 21, black, item['homeTeam']['teamName'])
                         oddsStartNum = awayTeamBlack + 8 if awayTeamBlack > homeTeamBlack else homeTeamBlack + 8
-                        graphics.DrawLine(canvas, 0, 1, oddsStartNum - 8, 1, blue)
-                        graphics.DrawLine(canvas, 0, 2, oddsStartNum - 8, 2, blue)
-                        graphics.DrawLine(canvas, 0, 3, oddsStartNum - 8, 3, blue)
-                        graphics.DrawLine(canvas, 0, 4, oddsStartNum - 8, 4, blue)
-                        graphics.DrawLine(canvas, 0, 5, oddsStartNum - 8, 5, blue)
-                        graphics.DrawLine(canvas, 0, 6, oddsStartNum - 8, 6, blue)
+                        for offset in range(19):
+                            graphics.DrawLine(canvas, 0, offset, oddsStartNum - 8, offset, blue)
                         awayTeam = graphics.DrawText(canvas, smallFont, 2, 11, awayColorSecondary, item['awayTeam']['teamName'])
                         homeTeam = graphics.DrawText(canvas, smallFont, 2, 21, homeColorSecondary, item['homeTeam']['teamName'])
                         runningCount = oddsStartNum
