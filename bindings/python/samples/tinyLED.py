fc7f929f909f12e00f6b6c54bfba200a248ae688
Patrick Bullion <pbullion@gmail.com>
2022-04-05 21:01:03 -0500

    u

--- bindings/python/samples/tinyLED.py
+++ bindings/python/samples/tinyLED.py
@@ -113,11 +113,12 @@ class RunText(SampleBase):
                         runningCount = runningCount + homeErrorTotal + 10
                         finalDetail = graphics.DrawText(canvas, middleFont, runningCount + 5, 20, yellow, 'F')
                         running = True
-                        pos += 1
+                        position = 40
+                        position += 1
                         while running:
-                            winnfingPitcher = graphics.DrawText(canvas, alilbiggerFont, pos, 30, green, "WP: {}".format(item['winningPitcher']))
-                            if (pos < 0):
-                                pos = offscreen_canvas.width
+                            winnfingPitcher = graphics.DrawText(canvas, alilbiggerFont, position, 30, green, "WP: {}".format(item['winningPitcher']))
+                            if (position < 0):
+                                # position = offscreen_canvas.width
                                 running = False
                             time.sleep(.01)
                     elif item['inprogress'] == True: 
