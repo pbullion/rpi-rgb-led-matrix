79c6707469504010913c1b5c28f1aa8b09e919f6
Patrick Bullion <pbullion@gmail.com>
2022-04-05 21:52:59 -0500

    u

--- bindings/python/samples/tinyLED.py
+++ bindings/python/samples/tinyLED.py
@@ -98,12 +98,12 @@ class RunText(SampleBase):
                         weatherTemp = graphics.DrawText(canvas, smallFont, runningCount + 5, 27, yellow, item['weather']['temp'])
                         startTime = graphics.DrawText(canvas, smallFont, runningCount, 30, yellow, item['startTime'])
                     if item['final'] == True:
-                        graphics.DrawLine(canvas, 0, 1, 64, 1, awayTeamColor)
-                        graphics.DrawLine(canvas, 0, 2, 64, 2, awayTeamColor)
-                        graphics.DrawLine(canvas, 0, 3, 64, 3, awayTeamColor)
-                        graphics.DrawLine(canvas, 0, 4, 64, 4, awayTeamColor)
-                        graphics.DrawLine(canvas, 0, 5, 64, 5, awayTeamColor)
-                        graphics.DrawLine(canvas, 0, 6, 64, 6, awayTeamColor)
+                        graphics.DrawLine(canvas, 0, 1, 64, 1, awayColorPrimary)
+                        graphics.DrawLine(canvas, 0, 2, 64, 2, awayColorPrimary)
+                        graphics.DrawLine(canvas, 0, 3, 64, 3, awayColorPrimary)
+                        graphics.DrawLine(canvas, 0, 4, 64, 4, awayColorPrimary)
+                        graphics.DrawLine(canvas, 0, 5, 64, 5, awayColorPrimary)
+                        graphics.DrawLine(canvas, 0, 6, 64, 6, awayColorPrimary)
                         awayTeam = graphics.DrawText(canvas, smallFont, 2, 11, awayColorSecondary, item['awayTeam']['teamName'])
                         homeTeam = graphics.DrawText(canvas, smallFont, 2, 21, homeColorSecondary, item['homeTeam']['teamName'])
                         oddsStartNum = awayTeam + 8 if awayTeam > homeTeam else homeTeam + 8
