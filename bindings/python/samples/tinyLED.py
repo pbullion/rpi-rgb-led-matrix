51f039725b89f75c099bf422bab0a9baaf8b1e92
Patrick Bullion <pbullion@gmail.com>
2022-04-05 22:06:21 -0500

    u

--- bindings/python/samples/tinyLED.py
+++ bindings/python/samples/tinyLED.py
@@ -106,7 +106,7 @@ class RunText(SampleBase):
                         for offset in range(13):
                             graphics.DrawLine(canvas, 0, offset + 13, oddsStartNum - 8, offset + 13, homeColorSecondary)
                         awayTeam = graphics.DrawText(canvas, smallFont, 1, 11, awayColorPrimary, item['awayTeam']['teamName'])
-                        homeTeam = graphics.DrawText(canvas, smallFont, 1, 22, homeColorPrimary, item['homeTeam']['teamName'])
+                        homeTeam = graphics.DrawText(canvas, smallFont, 1, 23, homeColorPrimary, item['homeTeam']['teamName'])
                         runningCount = oddsStartNum
                         awayScore = graphics.DrawText(canvas, smallFont, runningCount, 12, green if int(item['awayTeam']['score']) > int(item['homeTeam']['score']) else red, item['awayTeam']['score'])
                         homeScore = graphics.DrawText(canvas, smallFont, runningCount, 23, green if int(item['homeTeam']['score']) > int(item['awayTeam']['score']) else red, item['homeTeam']['score'])
@@ -118,7 +118,7 @@ class RunText(SampleBase):
                         awayErrorTotal = graphics.DrawText(canvas, smallFont, runningCount, 23, green if int(item['homeTeam']['score']) > int(item['awayTeam']['score']) else red, item['homeTeam']['errors']['displayValue'])
                         runningCount = runningCount + homeErrorTotal + 10
                         finalDetail = graphics.DrawText(canvas, middleFont, runningCount + 5, 20, yellow, 'F')
-                        winningPitcher = graphics.DrawText(canvas, alilbiggerFont, 0, 31, green, "WP: {}".format(item['winningPitcher']))
+                        winningPitcher = graphics.DrawText(canvas, alilbiggerFont, 0, 32, green, "WP: {}".format(item['winningPitcher']))
                     elif item['inprogress'] == True: 
                         situationString = '{}-{}'.format(item['situation']['balls'], item['situation']['strikes'])
                         baseSize = 6
