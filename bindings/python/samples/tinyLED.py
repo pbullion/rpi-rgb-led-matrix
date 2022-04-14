8b232bb37d87548aa060aa3e2b57044cedf26558
Patrick Bullion <pbullion@gmail.com>
2022-04-14 12:12:06 -0500

    u

--- bindings/python/samples/tinyLED.py
+++ bindings/python/samples/tinyLED.py
@@ -353,7 +353,7 @@ class RunText(SampleBase):
                     if 'RAIN' in item['condition'] or 'rain' in item['condition']:
                         canvas.SetImage(rainImage, 0, 0)
                         color = blue
-                    elif 'Cloudy' in item['condition'] or 'Overcast' in item['condition'] or 'cloudy' in item['condition'] or 'overcast' in item['condition']:
+                    elif 'CLOUDY' in item['condition'] or 'Overcast' in item['condition'] or 'cloudy' in item['condition'] or 'overcast' in item['condition']:
                         canvas.SetImage(partlyCloudyImage, 0, 0)
                         color = yellow
                     elif 'Thunder' in item['condition'] or 'thunder' in item['condition']:
