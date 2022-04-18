9442367d93da712238a5ea924f2639f39e0474c8
Patrick Bullion <pbullion@gmail.com>
2022-04-18 14:57:18 -0500

    u

--- bindings/python/samples/tinyLED.py
+++ bindings/python/samples/tinyLED.py
@@ -394,7 +394,7 @@ class RunText(SampleBase):
                         if (pos + topGolfers < 0):
                             running = False
                             pos = offscreen_canvas.width
-                        time.sleep(0.1)
+                        time.sleep(0.08)
                 else:
                     currentTIme = graphics.DrawText(canvas, font, 0, 23, blue, item)
                 time.sleep(5)
