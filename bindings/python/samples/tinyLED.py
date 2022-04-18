4af325744ad094f3c7f487a88ca808032e0ad8f1
Patrick Bullion <pbullion@gmail.com>
2022-04-18 15:23:07 -0500

    u

--- bindings/python/samples/tinyLED.py
+++ bindings/python/samples/tinyLED.py
@@ -388,10 +388,9 @@ class RunText(SampleBase):
                     while running:
                         offscreen_canvas.Clear()
                         tournamentNameBlack = graphics.DrawText(canvas, slightlyBiggerFont, 0, 10, black, item['tourneyName'])
-                        print(tournamentNameBlack)
                         tourneyStatusBlack = graphics.DrawText(canvas, smallestFont, 0, 16, black, item['status'])
-                        nameCentered = (tournamentNameBlack / 2)
-                        statusCentered = (tourneyStatusBlack / 2)
+                        nameCentered = 75 - (tournamentNameBlack / 2)
+                        statusCentered = 75 - (tourneyStatusBlack / 2)
                         tournamentName = graphics.DrawText(canvas, slightlyBiggerFont, nameCentered, 10, green, item['tourneyName'])
                         tourneyStatus = graphics.DrawText(canvas, smallestFont, statusCentered, 16, blue, item['status'])
                         pos -= 1
