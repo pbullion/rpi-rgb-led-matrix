e95a09be3a2a0a2f9e929ef64f914d923a0094c9
Patrick Bullion <pbullion@gmail.com>
2022-04-19 18:10:04 -0500

    u

--- bindings/python/samples/tinyLED.py
+++ bindings/python/samples/tinyLED.py
@@ -91,7 +91,7 @@ class RunText(SampleBase):
                         overOddsPoints = graphics.DrawText(canvas, smallestFont, runningCount, 10, green, 'O/U')
                         underOddsPoints = graphics.DrawText(canvas, alilbiggerFont, runningCount, 22, green, str(underTotalPointsString))
                         runningCount = runningCount + underOddsPoints + 4
-                        awayPitcher = graphics.DrawText(canvas, smallestFont, 0, 31, yellow, 'J. Verlander 3-1 2.50 ERA')
+                        awayPitcher = graphics.DrawText(canvas, smallestFont, 0, 31, white, 'J. Verlander 3-1 2.50')
                         startTime = graphics.DrawText(canvas, smallestFont, 100, 31, yellow, item['startTime'])
                     elif item['final'] == True:
                         awayTeamBlack = graphics.DrawText(canvas, smallFont, 1, 11, black, item['awayTeam']['teamName'])
