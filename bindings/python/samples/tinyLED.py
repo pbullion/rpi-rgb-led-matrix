66faa89dde5a90ca0d03632c6dce788880490554
Patrick Bullion <pbullion@gmail.com>
2022-04-20 10:39:14 -0500

    u

--- bindings/python/samples/tinyLED.py
+++ bindings/python/samples/tinyLED.py
@@ -464,9 +464,9 @@ class RunText(SampleBase):
                 elif type(item) is dict and item['type'] == 'time':
                     currentDayBlack = graphics.DrawText(canvas, alilbiggerFont, 150, 5, black, item['day'])
                     currentDateBlack = graphics.DrawText(canvas, middleFont, 150, 23, black, item['date'])
-                    currentTime = graphics.DrawText(canvas, font, 0, 23, blue, item['time'])
-                    currentDay = graphics.DrawText(canvas, slightlyBiggerFont, 127 - currentDayBlack, 10, blue, item['day'])
-                    currentDate = graphics.DrawText(canvas, slightlyBiggerFont, 127 - currentDateBlack, 23, blue, item['date'])
+                    currentTime = graphics.DrawText(canvas, font, 0, 21, blue, item['time'])
+                    currentDay = graphics.DrawText(canvas, slightlyBiggerFont, 75 - (currentDayBlack / 2), 32, blue, item['day'])
+                    currentDate = graphics.DrawText(canvas, slightlyBiggerFont, 127 - currentDateBlack, 14, blue, item['date'])
                     time.sleep(15)
                 else:
                     currentTime = graphics.DrawText(canvas, font, 0, 23, blue, item)
