2bcbd9747cbc9b94083a1e1fe358b770652e2aea
Patrick Bullion <pbullion@gmail.com>
2022-04-20 14:50:53 -0500

    u

--- bindings/python/samples/tinyLED.py
+++ bindings/python/samples/tinyLED.py
@@ -44,8 +44,8 @@ class RunText(SampleBase):
             # responseArr = [{'stockSymbol': 'TSLA', 'url': 'https://logo.clearbit.com/tesla.com', 'currentPrice': '999.58', 'up': False, 'percentChange': '2.78%'}, {'stockSymbol': 'OBNK', 'url': 'https://logo.clearbit.com/originbank.com', 'currentPrice': '42.40', 'up': True, 'percentChange': '1.56%'}, {'stockSymbol': 'AAPL', 'url': 'https://logo.clearbit.com/apple.com', 'currentPrice': '167.95', 'up': True, 'percentChange': '0.33%'}, {'stockSymbol': 'TWTR', 'url': 'https://logo.clearbit.com/twitter.com', 'currentPrice': '45.70', 'up': False, 'percentChange': '0.99%'}, {'stockSymbol': 'BTC', 'url': 'https://logo.clearbit.com/bitcoin.org', 'currentPrice': '41637', 'up': True, 'percentChange': '0.19%'}, {'stockSymbol': 'ETH', 'url': 'https://logo.clearbit.com/ethereum.org', 'currentPrice': '3103.48', 'up': False, 'percentChange': '0.32%'}, {'stockSymbol': 'DOGE', 'url': 'https://logo.clearbit.com/dogecoin.com', 'currentPrice': '0.1426', 'up': True, 'percentChange': '0.55%'}, {'league': 'nba', 'pregame': False, 'inprogress': False, 'final': True, 'awayTeam': {'teamName': 'Hawks', 'name': 'ATL', 'score': '105', 'colors': {'main': [200, 16, 46], 'secondary': [255, 255, 255]}, 'record': '27-14'}, 'homeTeam': {'teamName': 'Heat', 'name': 'MIA', 'score': '115', 'colors': {'main': [134, 38, 51], 'secondary': [0, 0, 0]}, 'record': '43-39'}, 'winningPitcher': '', 'losingPitcher': '', 'finalDetail': 'Final'}, {'league': 'nba', 'pregame': False, 'inprogress': False, 'final': True, 'awayTeam': {'teamName': 'Timberwolves', 'name': 'MIN', 'score': '96', 'colors': {'main': [0, 43, 92], 'secondary': [122, 193, 67]}, 'record': '26-15'}, 'homeTeam': {'teamName': 'Grizzlies', 'name': 'MEM', 'score': '124', 'colors': {'main': [35, 55, 91], 'secondary': [97, 137, 185]}, 'record': '46-36'}, 'winningPitcher': '', 'losingPitcher': '', 'finalDetail': 'Final'}, {'league': 'nba', 'pregame': False, 'inprogress': False, 'final': True, 'awayTeam': {'teamName': 'Pelicans', 'name': 'NO', 'score': '125', 'colors': {'main': [0, 43, 92], 'secondary': [227, 25, 55]}, 'record': '19-22'}, 'homeTeam': {'teamName': 'Suns', 'name': 'PHX', 'score': '114', 'colors': {'main': [229, 96, 32], 'secondary': [29, 17, 96]}, 'record': '36-46'}, 'winningPitcher': '', 'losingPitcher': '', 'finalDetail': 'Final'}, '']
             print(responseArr)
             canvas = self.matrix
-            bases =  [[113,5],[108,0],[103,5]]
-            outs = [[106,20],[112,20]]
+            bases =  [[115,5],[110,0],[105,5]]
+            outs = [[108,20],[114,20]]
             for item in responseArr:
                 print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
                 print(item)
@@ -186,8 +186,8 @@ class RunText(SampleBase):
                         homeErrorTotal = graphics.DrawText(canvas, smallFont, runningCount, 11, green if int(item['awayTeam']['score']) > int(item['homeTeam']['score']) else red, item['awayTeam']['errors']['displayValue'])
                         awayErrorTotal = graphics.DrawText(canvas, smallFont, runningCount, 24, green if int(item['homeTeam']['score']) > int(item['awayTeam']['score']) else red, item['homeTeam']['errors']['displayValue'])
                         runningCount = runningCount + homeErrorTotal + 5
-                        count = graphics.DrawText(canvas, smallestFont, 106, 19, yellow, situationString)
-                        inning = graphics.DrawText(canvas, smallestFont, 99, 31, yellow, item['inning'])
+                        count = graphics.DrawText(canvas, smallestFont, 108, 19, yellow, situationString)
+                        inning = graphics.DrawText(canvas, smallestFont, 101, 31, yellow, item['inning'])
                         time.sleep(5)
                     elif item['postponed'] == True:
                         awayTeamBlack = graphics.DrawText(canvas, smallFont, 1, 11, black, item['awayTeam']['teamName'])
