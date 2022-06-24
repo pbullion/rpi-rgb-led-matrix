870455b2494cd41cf06e26ea6e1805f253b70dac
Patrick Bullion <pbullion@gmail.com>
2022-06-24 17:45:59 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -76,6 +76,21 @@ class RunText(SampleBase):
                                 else:
                                     gameStatus = graphics.DrawText(offscreen_canvas, smallFont, pos + offset + test + buffer + buffer + buffer + buffer + buffer+ homeTeam + homeTeamStatus, 26, white, statusString)
                                     oddsStatus = graphics.DrawText(offscreen_canvas, smallFont, pos + offset + test + buffer + buffer + buffer + buffer + buffer+ homeTeam + homeTeamStatus, 10, white, oddsString)
+                            if 'inprogress' in game[0]: 
+                                offscreen_canvas.SetImage(awayLogo, pos, -10)
+                                versus = graphics.DrawText(offscreen_canvas, middleFont, pos + buffer + awayLogo.width, 24, green, 'vs')
+                                offscreen_canvas.SetImage(homeLogo, pos + awayLogo.width + buffer + buffer, -10)
+                                test = awayLogo.width + homeLogo.width + versus + buffer
+                                awayTeam = graphics.DrawText(offscreen_canvas, smallFont, pos + offset + test + buffer + buffer + buffer, 10, white, awayTeamString)
+                                awayTeamStatus = graphics.DrawText(offscreen_canvas, smallestFont, pos + offset + test + buffer + buffer + buffer + buffer + awayTeam, 10, white, awayTeamStatusString)
+                                homeTeam = graphics.DrawText(offscreen_canvas, smallFont, pos + offset + test + buffer + buffer + buffer, 26, white, homeTeamString)
+                                homeTeamStatus = graphics.DrawText(offscreen_canvas, smallestFont, pos + offset + test + buffer + buffer + buffer + buffer + homeTeam, 26, white, homeTeamStatusString)
+                                if awayTeam > homeTeam:
+                                    gameStatus = graphics.DrawText(offscreen_canvas, smallFont, pos + offset + test + buffer + buffer + buffer + buffer + buffer + awayTeam + awayTeamStatus, 26, white, statusString)
+                                    oddsStatus = graphics.DrawText(offscreen_canvas, smallFont, pos + offset + test + buffer + buffer + buffer + buffer + buffer+ awayTeam + awayTeamStatus, 10, white, oddsString)
+                                else:
+                                    gameStatus = graphics.DrawText(offscreen_canvas, smallFont, pos + offset + test + buffer + buffer + buffer + buffer + buffer+ homeTeam + homeTeamStatus, 26, white, statusString)
+                                    oddsStatus = graphics.DrawText(offscreen_canvas, smallFont, pos + offset + test + buffer + buffer + buffer + buffer + buffer+ homeTeam + homeTeamStatus, 10, white, oddsString)
                             if awayTeam > homeTeam:
                                 offset = offset + awayTeam + awayTeamStatus + gameStatus + oddsStatus + test + 60
                             else:
