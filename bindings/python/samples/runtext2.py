d94896ae9bf715d86026c8b3cdd06fbfb8c768f1
Patrick Bullion <pbullion@gmail.com>
2022-06-23 22:35:00 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -55,7 +55,7 @@ class RunText(SampleBase):
                             homeTeamString = game[10]
                             awayTeam = graphics.DrawText(offscreen_canvas, smallFont, pos + offset + buffer + buffer + buffer, 10, white, awayTeamString)
                             homeTeam = graphics.DrawText(offscreen_canvas, smallFont, pos + offset + buffer + buffer + buffer, 26, white, homeTeamString)
-                            offset = offset + 1
+                            offset = offset + 50
                     time.sleep(0.05)
                     offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
