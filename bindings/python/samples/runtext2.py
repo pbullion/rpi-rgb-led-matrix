9b3a0dc8074ac0fe7711c4b5d5d8aaf75177c358
Patrick Bullion <pbullion@gmail.com>
2022-06-30 15:14:34 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -89,6 +89,7 @@ class RunText(SampleBase):
                 running = True
                 print('-------------------------------')
                 print(arr)
+                print(arr[0])
                 while running:
                     offscreen_canvas.Clear()
                     buffer = 6
@@ -285,7 +286,7 @@ class RunText(SampleBase):
                             running = False
                             pos = offscreen_canvas.width
                         time.sleep(0.01)
-                    elif 'golf' in arr[0]:
+                    elif 'golf' === arr[0]:
                         print(arr[0])
                         versus = graphics.DrawText(offscreen_canvas, smallFont, 100, 12, red, arr[1])
                         length = graphics.DrawText(offscreen_canvas, smallFont, pos, 26, red, arr[3])
