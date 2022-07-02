d5ba5767421d84abd23ce2914947560e0e5f54a3
Patrick Bullion <pbullion@gmail.com>
2022-07-02 12:12:09 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -221,7 +221,7 @@ class RunText(SampleBase):
                                     y = outs[0][1]
                                     size = 4
                                     half = round(abs(6/2))
-                                    for testing in range(2):
+                                    for testing in range(1, half):
                                         graphics.DrawLine(offscreen_canvas, pos + offset + runningTotal + x - testing, y + size - testing, pos + offset + runningTotal + x + testing, y + size - testing, green)
                                         graphics.DrawLine(offscreen_canvas, pos + offset + runningTotal + x - testing, y + testing, pos + offset + runningTotal + x + testing, y + testing, green)
                                 if outsString == 2:
