7eb6d9b1ab3712513c924d05a52d3c52dd1d9af4
Patrick Bullion <pbullion@gmail.com>
2022-07-18 20:48:39 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -291,7 +291,7 @@ class RunText(SampleBase):
                         if (pos + length < 0):
                             running = False
                             pos = offscreen_canvas.width
-                        time.sleep(0.04)
+                        time.sleep(0.02)
                     elif arr == False:
                         running = False
                         pos = offscreen_canvas.width
