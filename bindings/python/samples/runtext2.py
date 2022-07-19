9e9207afc3cfb538bcbaebaeac4d99310d370ca0
Patrick Bullion <pbullion@gmail.com>
2022-07-18 21:22:37 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -310,7 +310,8 @@ class RunText(SampleBase):
                     elif isinstance(arr, list) and 'rssFeed' in arr[0]:
                         print(offscreen_canvas.width)
                         blackVs = graphics.DrawText(offscreen_canvas, bFont, 0, 12, black, arr[1])
-                        versus = graphics.DrawText(offscreen_canvas, bFont, 175, 12, blue, arr[1])
+                        print(blackVs)
+                        versus = graphics.DrawText(offscreen_canvas, bFont, offscreen_canvas.width / 2 + blackVs, 12, blue, arr[1])
                         length = graphics.DrawText(offscreen_canvas, bFont, pos, 26, green, arr[2])
                         pos -= 1
                         if (pos + length < 0):
