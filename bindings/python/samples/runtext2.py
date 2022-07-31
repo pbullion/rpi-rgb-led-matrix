24c12a6ea5473ae3a977098e35eb59527243b2df
Patrick Bullion <pbullion@gmail.com>
2022-07-31 14:39:51 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -94,6 +94,8 @@ class RunText(SampleBase):
                     buffer = 6
                     pos -= 1
                     offset = 0
+                    if isinstance(arr, list)
+                        print(arr[0])
                     if isinstance(arr, list) and 'mlb' in arr[0]:
                         for game in arr:
                             if 'mlb logo' in game[0]:
