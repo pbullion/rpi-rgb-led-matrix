e10f322872596a3d47e165aa1cbc0d2759421499
Patrick Bullion <pbullion@gmail.com>
2022-07-31 16:23:38 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -303,7 +303,7 @@ class RunText(SampleBase):
                             pos = offscreen_canvas.width
                     elif isinstance(arr, list) and 'rssFeed' in arr[0]:
                         blackVs = graphics.DrawText(offscreen_canvas, bFont, -1000, 12, green, arr[1])
-                        versus = graphics.DrawText(offscreen_canvas, bFont, ((offscreen_canvas.width / 2) + (blackVs)), 12, blue, arr[1])
+                        versus = graphics.DrawText(offscreen_canvas, bFont, ((offscreen_canvas.width / 2) - (blackVs / 2)), 12, blue, arr[1])
                         length = graphics.DrawText(offscreen_canvas, bFont, pos, 26, green, arr[2])
                         pos -= 1
                         if (pos + length < 0):
@@ -328,8 +328,8 @@ class RunText(SampleBase):
                         time.sleep(0.020)
                     elif isinstance(arr, list) and arr[0] == 'golf':
                         blackVs = graphics.DrawText(offscreen_canvas, bFont, -1000, 12, green, arr[1])
-                        versus = graphics.DrawText(offscreen_canvas, bFont, (offscreen_canvas.width / 2), 12, blue, arr[1])
-                        versus2 = graphics.DrawText(offscreen_canvas, bFont,(offscreen_canvas.width / 2)+ versus + 25, 12, blue, arr[2])
+                        versus = graphics.DrawText(offscreen_canvas, bFont, ((offscreen_canvas.width / 2) - (blackVs / 2)), 12, blue, arr[1])
+                        # versus2 = graphics.DrawText(offscreen_canvas, bFont,(offscreen_canvas.width / 2)+ versus + 25, 12, blue, arr[2])
                         length = graphics.DrawText(offscreen_canvas, bFont, pos, 26, green, arr[3])
                         pos -= 1
                         time.sleep(0.020)
