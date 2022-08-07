7ae1e6a4275c9a4622a136032e56270bba2fcf80
Patrick Bullion <pbullion@gmail.com>
2022-08-07 14:41:19 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -9,7 +9,7 @@ import json
 import random
 from PIL import Image
 import requests
-userFile = open('data.json')
+userFile = open('user.json')
 
 class RunText(SampleBase):
     def __init__(self, *args, **kwargs):
