79a7a69b8982bdaf4bd8e13e4803bc718951660b
Patrick Bullion <pbullion@gmail.com>
2023-01-06 15:36:57 -0600

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -2306,9 +2306,9 @@ class RunText(SampleBase):
                                     overUnderString,
                                 )
                             if "pregame" in game[0]:
-                                offset = offset + 190
+                                offset = offset + 150
                             if "inProgress" in game[0]:
-                                offset = offset + 190
+                                offset = offset + 150
                             if "final" in game[0]:
                                 awayTeamString = game[5]
                                 homeTeamString = game[10]
