27bf7f93360dcd5049acdeee0c2547b798674bd6
Patrick Bullion <pbullion@gmail.com>
2023-01-06 12:19:47 -0600

    u

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -20,7 +20,7 @@ class RunText(SampleBase):
 
         while True:
             offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 26, textColor, my_text)
+            len = graphics.DrawText(offscreen_canvas, font, pos, 20, textColor, my_text)
             pos -= 1
             if (pos + len < 0):
                 pos = offscreen_canvas.width
