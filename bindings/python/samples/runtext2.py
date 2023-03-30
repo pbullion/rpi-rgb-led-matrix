23294f333d4d166232ef4eaf3690b661bbe76bb9
Patrick Bullion <pbullion@gmail.com>
2023-03-29 20:14:26 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -1688,7 +1688,7 @@ class RunText(SampleBase):
                                 offset = offset + 190
                             if "inProgress" in game[0]:
                                 offset = offset + 190
-                        time.sleep(0.018)
+                        time.sleep(0.016)
                         if pos + offset < 0:
                             running = False
                             pos = offscreen_canvas.width
