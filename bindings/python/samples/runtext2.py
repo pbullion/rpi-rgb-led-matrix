19c0d12f3ce2a816f9f5c671272a29361d27b2a3
Patrick Bullion <pbullion@gmail.com>
2023-04-12 23:06:27 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -181,7 +181,7 @@ class RunText(SampleBase):
             .resize((50, 50), Image.ANTIALIAS),
             "Los Angeles Dodgers": Image.open(
                 requests.get(
-                    "https://image.pngaaa.com/445/3685445-middle.png",
+                    "https://img.favpng.com/0/9/0/dodger-stadium-los-angeles-dodgers-los-angeles-angels-mlb-world-series-png-favpng-xXdVneTLUfXjHKGP1KXqd8QUC.jpg",
                     stream=True,
                 ).raw
             )
