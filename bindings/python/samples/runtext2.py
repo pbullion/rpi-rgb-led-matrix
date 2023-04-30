3f2c39a662c8f35ac90182a4cf8b76be0bc8dd04
Patrick Bullion <pbullion@gmail.com>
2023-04-30 14:49:58 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -5479,7 +5479,7 @@ class RunText(SampleBase):
                         versus = graphics.DrawText(
                             offscreen_canvas,
                             bFont,
-                            ( (offscreen_canvas.width)-(blackVs / 2)),
+                            ((offscreen_canvas.width / 2) - (blackVs)),
                             12,
                             blue,
                             arr[1],
