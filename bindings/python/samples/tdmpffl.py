4ae95313532ef8b729ff164800dfa56bf02e4647
Patrick Bullion <pbullion@gmail.com>
2023-08-10 21:55:48 -0500

    u

--- bindings/python/samples/tdmpffl.py
+++ bindings/python/samples/tdmpffl.py
@@ -8,7 +8,12 @@ import time
 class RunText(SampleBase):
     def __init__(self, *args, **kwargs):
         super(RunText, self).__init__(*args, **kwargs)
-        self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
+        self.parser.add_argument(
+            "-t",
+            "--text",
+            help="The text to scroll on the RGB LED panel",
+            default="Hello world!",
+        )
 
     def run(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
@@ -17,12 +22,26 @@ class RunText(SampleBase):
         textColor = graphics.Color(255, 255, 0)
         pos = offscreen_canvas.width
         my_text = self.args.text
-
+        currentRound = 1
+        currentPick = 1
+        leagueMembers = [
+            "Patrick",
+            "Mike",
+            "Jim",
+            "Jason",
+            "Josh",
+            "Jeff",
+        ]
         while True:
             offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 26, textColor, my_text)
+            text_string = (
+                currentRound + "." + currentPick + " " + leagueMembers[currentPick - 1]
+            )
+            len = graphics.DrawText(
+                offscreen_canvas, font, pos, 26, textColor, text_string
+            )
             pos -= 1
-            if (pos + len < 0):
+            if pos + len < 0:
                 pos = offscreen_canvas.width
 
             time.sleep(0.01)
@@ -32,5 +51,5 @@ class RunText(SampleBase):
 # Main function
 if __name__ == "__main__":
     run_text = RunText()
-    if (not run_text.process()):
+    if not run_text.process():
         run_text.print_help()
