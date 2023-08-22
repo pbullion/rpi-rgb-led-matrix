a9bc452e0dbc2337d43b968ae4890eae0bafd738
Patrick Bullion <pbullion@gmail.com>
2023-08-22 14:58:47 -0500

    u

--- bindings/python/samples/tdmpffl.py
+++ bindings/python/samples/tdmpffl.py
@@ -309,7 +309,7 @@ class RunText(SampleBase):
                         if currentRound % 2 != 0:
                             currentPickIndex += 1
                             currentPick += 1
-                offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+                    offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
         finally:
             # shut down cleanly
             curses.nocbreak()
