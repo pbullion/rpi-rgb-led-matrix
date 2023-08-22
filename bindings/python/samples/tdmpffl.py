b1ac5db9640130597b2b3a6f0f96cd6683fddeb0
Patrick Bullion <pbullion@gmail.com>
2023-08-22 15:16:29 -0500

    u

--- bindings/python/samples/tdmpffl.py
+++ bindings/python/samples/tdmpffl.py
@@ -146,6 +146,7 @@ class RunText(SampleBase):
                         if currentRound % 2 != 0:
                             currentPickIndex += 1
                             currentPick += 1
+                        break
                     curses.napms(1000)
                     round_text = "Rd " + str(currentRound) + "." + str(currentPick)
                     seconds -= 1
