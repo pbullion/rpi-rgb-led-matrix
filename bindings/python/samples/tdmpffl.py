0e6903b81e011aa6d2e1eec311e30532a8af68f5
Patrick Bullion <pbullion@gmail.com>
2023-08-22 14:43:36 -0500

    u

--- bindings/python/samples/tdmpffl.py
+++ bindings/python/samples/tdmpffl.py
@@ -133,6 +133,7 @@ class RunText(SampleBase):
                 print("Current Pick Index:" + str(currentPickIndex))
                 offscreen_canvas.Clear()
                 round_text = "Rd " + str(currentRound) + "." + str(currentPick)
+                time.sleep(1)
                 seconds -= 1
                 timeColor = green
                 if seconds < 60:
