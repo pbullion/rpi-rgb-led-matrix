911801c61353eee45d4f433799207ef2c5f879b9
Patrick Bullion <pbullion@gmail.com>
2023-08-25 15:12:34 -0500

    u

--- bindings/python/samples/olympics.py
+++ bindings/python/samples/olympics.py
@@ -93,7 +93,7 @@ class RunText(SampleBase):
                     10,
                     26,
                     green,
-                    "GAYLEB",
+                    "Donny",
                 )
                 if final > 0:
                     remainingTime = graphics.DrawText(
