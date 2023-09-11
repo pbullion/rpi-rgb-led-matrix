2629419a8879ebf1a79fd5a8af88e87ab0327ba5
Patrick Bullion <pbullion@gmail.com>
2023-09-11 14:30:14 -0500

    u

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -8,29 +8,34 @@ import time
 class RunText(SampleBase):
     def __init__(self, *args, **kwargs):
         super(RunText, self).__init__(*args, **kwargs)
-        self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
+        self.parser.add_argument(
+            "-t",
+            "--text",
+            help="The text to scroll on the RGB LED panel",
+            default="Hello world!",
+        )
 
     def run(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
-        font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/texgyre-27.bdf")
+        font.LoadFont("../../../fonts/7x13.bdf")
         textColor = graphics.Color(255, 255, 0)
         pos = offscreen_canvas.width
         my_text = self.args.text
 
         while True:
             offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 26, textColor, my_text)
+            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
             pos -= 1
-            if (pos + len < 0):
+            if pos + len < 0:
                 pos = offscreen_canvas.width
 
-            time.sleep(0.01)
+            time.sleep(0.05)
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
 
 # Main function
 if __name__ == "__main__":
     run_text = RunText()
-    if (not run_text.process()):
+    if not run_text.process():
         run_text.print_help()
