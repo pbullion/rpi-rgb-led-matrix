32920b6b3d1db58a3fffca77a10f101a145a063b
Patrick Bullion <pbullion@gmail.com>
2024-02-13 10:42:48 -0600

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -3640,8 +3640,6 @@ class RunText(SampleBase):
                                 awaySpreadString = game[22]
                                 homeSpreadString = game[23]
                                 overUnderText = ""
-                                print(re.sub(pattern, "", awayTeamString))
-                                print(re.sub(pattern, "", homeTeamString))
                                 if overUnderString != "":
                                     overUnderText = "O/U"
                                 offscreen_canvas.SetImage(
