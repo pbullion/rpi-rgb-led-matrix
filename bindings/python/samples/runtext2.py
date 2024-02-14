1cb4e0d3377f96d1c65ecb517678c10cad172bb2
Patrick Bullion <pbullion@gmail.com>
2024-02-13 20:22:16 -0600

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -4393,7 +4393,7 @@ class RunText(SampleBase):
                                     + awayTeam
                                     + awayTeamStatus
                                     + headlineString
-                                    + 200
+                                    + 250
                                 )
                             else:
                                 offset = (
@@ -4401,7 +4401,7 @@ class RunText(SampleBase):
                                     + homeTeam
                                     + homeTeamStatus
                                     + headlineString
-                                    + 200
+                                    + 250
                                 )
                         time.sleep(0.018)
                         if pos + offset < 0:
