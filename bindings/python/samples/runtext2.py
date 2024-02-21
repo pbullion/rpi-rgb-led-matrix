11f84ac2004ff7915aef9769cd41e5010390e8ae
Patrick Bullion <pbullion@gmail.com>
2024-02-20 21:06:56 -0600

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -5275,7 +5275,10 @@ class RunText(SampleBase):
                                     white,
                                     teeTime,
                                 )
-                                offset = offset + athleteNameText + 150 + newBuffer
+                            if "pregame" in game[0]:
+                                offset = offset + 100 + newBuffer
+                            else:
+                                offset = offset + homeTeam + 100 + newBuffer
                         time.sleep(0.018)
                         if pos + offset < 0:
                             running = False
