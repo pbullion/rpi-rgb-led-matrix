3db9ec3ad040128c6a6b73b9ead73067c618af25
Patrick Bullion <pbullion@gmail.com>
2024-02-28 22:00:08 -0600

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -3038,7 +3038,7 @@ class RunText(SampleBase):
                 "/home/pi/rpi-rgb-led-matrix/bindings/python/samples/images/logos/f1/teams/ferrari-logo Background Removed.png"
             )
             .convert("RGB")
-            .resize((30, 40), Image.ANTIALIAS),
+            .resize((45, 30), Image.ANTIALIAS),
             "Kick Sauber": Image.open(
                 "/home/pi/rpi-rgb-led-matrix/bindings/python/samples/images/logos/f1/teams/kick-sauber-logo Background Removed.png"
             )
