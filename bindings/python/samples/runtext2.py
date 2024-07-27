3d5de221eec4f9ca4c9dfd43359c4e96ffde6fb8
Patrick Bullion <pbullion@gmail.com>
2024-07-27 18:34:12 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -8598,7 +8598,7 @@ class RunText(SampleBase):
                                     bronze,
                                     country.n_Bronze,
                                 )
-                            offset = offset + countryName+ 60
+                            offset = offset + 200+ 60
                         time.sleep(0.018)
                         if pos + offset < 0:
                             running = False
