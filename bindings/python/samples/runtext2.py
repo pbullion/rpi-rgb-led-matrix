f3f4a2a6fad61481242e39e2a8cbe7e99c89855b
Patrick Bullion <pbullion@gmail.com>
2024-07-27 18:38:40 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -8560,9 +8560,8 @@ class RunText(SampleBase):
                             pos = offscreen_canvas.width
                     elif isinstance(arr, list) and "olympicMedals" in arr[0]:
                         for country in arr:
-                            print(country)
                             if country != "olympicMedals":
-                                print(country)
+                                print(country.c_NOCShort)
                                 offscreen_canvas.SetImage(
                                     olympicCountries[country.c_NOCShort],
                                     pos + offset,
