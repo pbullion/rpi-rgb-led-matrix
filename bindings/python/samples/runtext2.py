eb95e8c36aae5107eab038a2d5b2be1f0b1edc00
Garage <garage@Garages-Mac-mini.local>
2024-09-04 21:04:36 -0500

    u

--- bindings/python/samples/runtext2.py
+++ bindings/python/samples/runtext2.py
@@ -8214,7 +8214,7 @@ class RunText(SampleBase):
                                     overUnderString,
                                 )
                             if "pregame" in game[0]:
-                                offset = offset + 165
+                                offset = offset + 155
                             if "inProgress" in game[0]:
                                 offset = offset + 190
                             if "final" in game[0]:
